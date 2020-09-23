d96c72f284d0ec42bb71feff79ac86130cdb4377
STEIN197 <nil20122013@gmail.com>
2020-09-23 21:23:43 +0500

    Subdivision is complete

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -4,22 +4,25 @@
 	$mapping->registerClasses();
 	SC\Options::init(2);
 	trait delete_it {
-		abstract function getProperties();
-		abstract function setProperties();
-		abstract function getProperty();
-		abstract function setProperty();
+
+		// trait Propertiable
+		abstract function getProperties(): array;
+		abstract function setProperties(array $properties);
+		abstract function getProperty(string $key): ?array;
+		abstract function setProperty(string $key, $value);
 		protected $arProperties;
 		
 		// abstract class Entity
 		protected $id;
 		protected $arFields;
-		abstract function getFields();
-		abstract function setFields();
-		abstract function getField();
-		abstract function setField();
+		abstract function getFields(): array;
+		abstract function setFields(array $arFields);
+		abstract function getField(string $key): ?array;
+		abstract function setField(string $key, $value);
 		function getID(): ?int {}
 		abstract function save(): void;
 		abstract function delete(): void;
+		abstract function make($entity); // Из $intID, ['ID'], Entity::$id
 		abstract static function getList(array $arFilter, array $arOrder = ['SORT' => 'ASC'], ?array $arSelect = null, ?array $arNav = null): array;
 		abstract static function getByID(int $id): ?self; // TODO: Только в трейтах
 		static function castTypes(array &$arFields): void {}
@@ -36,13 +39,12 @@
 		abstract function getParent();
 		abstract function setParent(Section $parent);
 
-		// class IBlock extends Entity implements EntityContainer
-		// class Section extends Entity implements EntityContainer use IBlockEntity, IBlockTreeEntity
-		
-		// class Element extends Entity use IBlockEntity, IBlockTreeEntity
+		// class IBlock extends Entity implements EntityContainer user Propertiable
+		// class Section extends Entity implements EntityContainer use IBlockEntity, IBlockTreeEntity, Propertiable
+
+		// class Element extends Entity use IBlockEntity, IBlockTreeEntity, Propertiable
 		abstract function getParents(): array;
 		abstract function setParents(): array;
 		
 		// class Property extends Entity use IBlockEntity
-		public static function make($property): Property {}
 	}
