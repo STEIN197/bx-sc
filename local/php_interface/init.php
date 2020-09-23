a203e1bbe1151e361e55cb515ab2decec553c422
STEIN197 <nil20122013@gmail.com>
2020-09-23 21:13:57 +0500

    Methods divided between classes

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -3,26 +3,45 @@
 	$mapping = new AutoloadMapping(__DIR__.DIRECTORY_SEPARATOR.'lib');
 	$mapping->registerClasses();
 	SC\Options::init(2);
-	interface delete_it {
-		function getIBlock();
-		function getParent();
-		function getChildren();
+	trait delete_it {
+		abstract function getFields();
+		abstract function getProperties();
+		abstract function setFields();
+		abstract function setProperties();
+		abstract function getField();
+		abstract function getProperty();
+		abstract function setField();
+		abstract function setProperty();
+		protected $arFields;
+		protected $arProperties;
+		
+		// abstract class Entity
+		protected $id;
+		function getID(): ?int {}
+		abstract function save(): void;
+		abstract function delete(): void;
+		abstract static function getList(array $arFilter, array $arOrder = ['SORT' => 'ASC'], ?array $arSelect = null, ?array $arNav = null): array;
+		abstract static function getByID(int $id): ?self; // TODO: Только в трейтах
+		static function castTypes(array &$arFields): void {}
+		
+		// interface EntityContainer
+		abstract function getElements(array $arFilter = [], array $arOrder = ['SORT' => 'ASC'], ?array $arSelect = null, ?array $arNav = null): array;
+		abstract function getSections(array $arFilter = [], array $arOrder = ['SORT' => 'ASC'], ?array $arSelect = null, ?array $arNav = null): array;
+		abstract function getDistinctValues($property, bool $includeInactive = true, array $arFilter = null);
+		
+		// trait IBlockEntity
+		abstract function getIBlock(): ?IBlock;
 
-		function getFields();
-		function getProperties();
-		function setFields();
-		function setProperties();
-		function getField();
-		function getProperty();
-		function setField();
-		function setProperty();
+		// trait IBlockTreeEntity
+		abstract function getParent();
+		abstract function setParent(Section $parent);
 
-		function getID(): int;
-		function save();
-		function delete();
-		static function getList();
-		static function castTypes(array $arFields);
-		static function getByID(int $id);
-		function getDistinctValues();
+		// class IBlock extends Entity implements EntityContainer
+		// class Section extends Entity implements EntityContainer use IBlockEntity, IBlockTreeEntity
+		// class Element extends Entity use IBlockEntity, IBlockTreeEntity
+		abstract function getParents(): array;
+		abstract function setParents(): array;
+		
+		// class Property extends Entity use IBlockEntity
+		public static function make($property): Property {}
 	}
-
