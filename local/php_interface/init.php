474fd1940f201418e9bd0871577918b537beedd5
STEIN197 <nil20122013@gmail.com>
2020-09-23 20:14:59 +0500

    Added test interface to remember all methods to implement

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -2,3 +2,26 @@
 	require 'AutoloadMapping.php';
 	$mapping = new AutoloadMapping(__DIR__.DIRECTORY_SEPARATOR.'lib');
 	$mapping->registerClasses();
+	interface delete_it {
+		function getIBlock();
+		function getParent();
+		function getChildren();
+
+		function getFields();
+		function getProperties();
+		function setFields();
+		function setProperties();
+		function getField();
+		function getProperty();
+		function setField();
+		function setProperty();
+
+		function getID(): int;
+		function save();
+		function delete();
+		static function getList();
+		static function castTypes(array $arFields);
+		static function getByID(int $id);
+		function getDistinctValues();
+	}
+
