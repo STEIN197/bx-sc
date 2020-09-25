fe43fb9e8ccacf6b2bbee37451ce94a219927a1f
Nail <nil20122013@gmail.com>
2020-09-25 15:01:00 +0500

    Created select method

--- local/php_interface/lib/SC/Query.php
+++ local/php_interface/lib/SC/Query.php
@@ -25,6 +25,7 @@
 			'rshift' => '>>',
 			'gte' => '>=',
 			'lt' => '<',
+			'eq' => '=',
 			'neq' => '!=',
 			'lshift' => '<<',
 			'lte' => '<=',
@@ -49,34 +50,35 @@
 		];
 
 		/** @var array */
-		private $query = [];
+		private $query;
 
 		public function __construct(...$arguments) {
-			// $this->query = $arguments;
+			$this->query = $arguments;
 		}
 
 		public function select(...$arguments): self {
 			$this->query[] = 'SELECT';
 			$argsCount = sizeof($arguments);
 			if ($argsCount) {
+				$arguments = array_map('self::trim', $arguments);
 				if ($argsCount === 1) {
 					if (is_array($arguments[0])) {
-
+						$select = [];
+						// var_dump($arguments);
+						foreach ($arguments[0] as $key => $value) {
+							if (is_string($key)) {
+								$select[] = self::filter($key, self::PREFIX_NAME).' AS '.self::filter($value, self::PREFIX_NAME);
+							} else {
+								$select[] = self::filter($value, self::PREFIX_NAME);
+							}
+						}
+						$this->query[] = join(', ', $select);
 					} else {
-
+						$this->query[] = self::filter($arguments[0], self::PREFIX_NAME);
 					}
 				} else {
-
+					$this->query[] = self::filter($arguments[0], self::PREFIX_NAME).' AS '.self::filter($arguments[1], self::PREFIX_NAME);
 				}
-				$arguments = array_map('strval', $arguments);
-				$arguments = array_map('self::trim', $arguments);
-				// ...
-				select('*');
-				select('s', 'd');
-				select([
-					'sd',
-					'cd' => '34'
-				]);
 			}
 			return $this;
 		}
@@ -96,24 +98,27 @@
 		public function __call($method, $arguments): ?self {
 			$lcMethod = strtolower($method);
 			if (isset(self::$operators[$lcMethod])) {
-				// return 
+				$this->query[] = self::$operators[$lcMethod];
+				$arguments = array_map('self::trim', $arguments);
+				foreach ($arguments as $arg)
+					$this->query[] = self::filter($arg);
+			} else {
+				$matches = null;
+				preg_match_all('/((?:^|[A-Z])[a-z]+)/', $method, $matches);
+				$method = join('_', $matches[0]);
+				$arguments = array_map('self::filter', $arguments);
+				$this->query[] = strtoupper($method).'('.join(', ', $arguments).')';
 			}
-			$ucMethod = strtoupper($method);
-			$arguments = array_map('strval', $arguments);
-			$arguments = array_map('self::escape', $arguments);
-			$arguments = array_map('self::enclose', $arguments);
-			// TODO: Проверка на колонки
-			$arguments = array_map('self::convertType', $arguments);
-			$this->query[] = $ucMethod.'('.join(', ', $arguments).')';
 			return $this;
 		}
 
 		public function __toString(): string {
+			// var_dump($this->query);
 			return join(' ', $this->query);
 		}
 
 		public static function new(...$arguments): self {
-			return new self($arguments);
+			return new self(...$arguments);
 		}
 
 		public static function setQueryHandler($callback): void {
@@ -124,7 +129,7 @@
 
 		private static function convertToSQLType($value): string {
 			if (is_string($value))
-				return "'{$value}'";
+				return '\''.self::escape($value).'\'';
 			if (is_null($value))
 				return 'NULL';
 			if (is_bool($value))
@@ -135,21 +140,24 @@
 		private static function filter($input, ?string $defaultPrefix = null): string {
 			if ($input instanceof self)
 				return (string) $input;
+				// return '('.((string) $input).')';
 			if (is_string($input)) {
 				if (!in_array($input{0}, self::$prefixes) && $defaultPrefix)
 					$input = $defaultPrefix.$input;
-				$prefix = $input{0};
-				$input = substr($input, 1);
-				if (in_array($prefix, self::$prefixes)) {
-					switch ($prefix) {
-						case self::PREFIX_NAME:
-							return join('.', array_map(function($value) {
-								return "`{$value}`";
-							}, explode('.', $input)));
-						case self::PREFIX_RAW:
-							return $input;
-						case self::PREFIX_STRING:
-							return "'{$input}'";
+				if (in_array($input{0}, self::$prefixes)) {
+					$prefix = $input{0};
+					$input = substr($input, 1);
+					if (in_array($prefix, self::$prefixes)) {
+						switch ($prefix) {
+							case self::PREFIX_NAME:
+								return join('.', array_map(function($value) {
+									return "`{$value}`";
+								}, explode('.', $input)));
+							case self::PREFIX_RAW:
+								return $input;
+							case self::PREFIX_STRING:
+								return "'{$input}'";
+						}
 					}
 				}
 				return "'{$input}'";
@@ -161,58 +169,71 @@
 			return addslashes($value);
 		}
 
-		private static function enclose($value): string {
-			
+		private static function trim($value) {
+			if (is_array($value)) {
+				$result = [];
+				foreach ($value as $k => $v) {
+					$result[$k] = self::trim($v);
+				}
+				return $result;
+			} elseif (is_string($value)) {
+				return trim($value);
+			} else {
+				return $value;
+			}
 		}
 	}
 
+	return;
 
+	Query::new()
+	->select('*')
+	->from(
 		Query::new()
-			->select('*')
+		->select([
+			Query::new()->substringIndex('@Steel', ': ', 1) => 'Steel_Type', // TODO: Ключи-объекты не разрешены
+			Query::new()->substringIndex('@Steel', ': ', -1)->as('Steel'),
+			Query::new()->min('@Steel_Size') => 'Min_Steel_Size',
+			Query::new()->max('@Steel_Size') => 'Max_Steel_Size',
+			Query::new()->groupConcat(Query::new()->distinct('@Steel_Size')->orderBy()->field('@Steel_Standart', 'ГОСТ')->separator(',')) => 'Max_Steel_Size',
+		])
+		->from('@Message236')
+		->where([
+			'@Subdivision_ID' => 99,
+			'@Checked'
+		])
+		->groupBy('@Steel')
+	, 'MainTable')
+	->orderBy(
+		Query::new()
+		->position('@Steel')
+		->in(
+			Query::new()
+			->select()->groupConcat(Query::new('@Subdivision_Name')->separator(','))
 			->from(
 				Query::new()
-					->select([
-						Query::new()->substringIndex('@Steel', ': ', 1) => 'Steel_Type', // TODO: Ключи-объекты не разрешены
-						Query::new()->substringIndex('@Steel', ': ', -1)->as('Steel'),
-						Query::new()->min('@Steel_Size') => 'Min_Steel_Size',
-						Query::new()->max('@Steel_Size') => 'Max_Steel_Size',
-						Query::new()->groupConcat(Query::new()->distinct('@Steel_Size')->orderBy()->field('@Steel_Standart', 'ГОСТ')->separator(',')) => 'Max_Steel_Size',
-					])
-					->from('@Message236')
-					->where([
-						'@Subdivision_ID' => 99,
-						'@Checked'
-					])
-					->groupBy('@Steel')
-			, 'MainTable')
-			->orderBy(
-				Query::new()->position('@Steel')->in(
-					Query::new()
-					->select()->groupConcat(Query::new('@Subdivision_Name')->separator(','))
-					->from(
-						Query::new()
-						->select([
-							'@Child.Subdivision_ID',
-							'@Child.Subdivision_Name',
-							'@Parent.Subdivision_Name' => 'Parent_Name',
-							'@Child.Parent_Sub_ID',
-							'@Parent.Priority' => 'Parent_Priority',
-							'@Child.Priority' => 'Child_Priority',
-						])
-						->from('@Subdivision', 'Child')
-							->leftJoin('@Subdivision', 'Parent')
-							->on('@Child.Parent_Sub_ID', '@Parent.Subdivision_ID')
-						->where([
-							'@Child.Catalogue_ID' => 6,
-							'@Parent.Parent_Sub_ID' => 120
-						])
-						->orderBy([
-							'@Parent.Priority',
-							'@Child.Priority',
-						]), 't1'
-					)
-				)
-			);
+				->select([
+					'@Child.Subdivision_ID',
+					'@Child.Subdivision_Name',
+					'@Parent.Subdivision_Name' => 'Parent_Name',
+					'@Child.Parent_Sub_ID',
+					'@Parent.Priority' => 'Parent_Priority',
+					'@Child.Priority' => 'Child_Priority',
+				])
+				->from('@Subdivision', 'Child')
+					->leftJoin('@Subdivision', 'Parent')
+					->on('@Child.Parent_Sub_ID', '@Parent.Subdivision_ID')
+				->where([
+					'@Child.Catalogue_ID' => 6,
+					'@Parent.Parent_Sub_ID' => 120
+				])
+				->orderBy([
+					'@Parent.Priority',
+					'@Child.Priority',
+				]), 't1'
+			)
+		)
+	);
 
 	// TODO
 	function select(): Query {}
