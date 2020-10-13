3342aab6b76a0fb38ed7dec149831d3cf9f8e043
Nail <nil20122013@gmail.com>
2020-10-13 11:09:08 +0500

    Added exception

---
+++ local/php_interface/lib/SC/Bitrix/EntityDatabaseException.php
@@ -0,0 +1,14 @@
+<?php
+	namespace SC\Bitrix;
+
+	class EntityDatabaseException extends \Exception {
+
+		/** @var Entity */
+		private $entity;
+
+		public function __construct($entity, string $message = '', int $code = 0) {
+			$this->entity = $entity;
+			$this->message = $message;
+			$this->code = $code;
+		}
+	}
